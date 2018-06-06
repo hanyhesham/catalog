7c6c455286b60e71889546dceed817f6fe6761a6
Hany Hesham <hany.hesham@itworx.com>
2018-06-06 15:31:11 +0300

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -14,7 +14,7 @@ class User(Base):
 
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(350), nullable=False)
     email = Column(String(300), nullable=False)
     # picture = Column(String(300))
 
@@ -23,7 +23,7 @@ class Categories(Base):
 
     __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(350), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
@@ -36,8 +36,8 @@ class CategoryItem(Base):
 
     __tablename__ = 'category_item'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    description = Column(String(250))
+    name = Column(String(350), nullable=False)
+    description = Column(String(350))
     categories_id = Column(Integer, ForeignKey('categories.id'))
     categories = relationship(Categories, cascade='all, delete-orphan',
                               single_parent=True)
